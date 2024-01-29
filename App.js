042a03ba4cb9b6c73e284ddc5fcf1b7c6601a112
Lindsay Wolfgramm <92181847+lwolfgramm@users.noreply.github.com>
2024-01-29 13:09:31 -0700

    initial file upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { Button } from "@mui/material";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button>Join the waitlist</Button>
+    </div>
+  );
+}
